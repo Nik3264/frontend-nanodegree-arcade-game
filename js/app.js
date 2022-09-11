1fd091fa77df2776d6c78ca2ee965e8923dc265b
Oksana <example@gmail.com>
2022-09-11 18:23:14 +0300

    extended collision capture coordinates

--- js/app.js
+++ js/app.js
@@ -1,20 +1,12 @@
-// Enemies our player must avoid
+
 CELL_HEIGHT=83;
 CELL_WIDTH=100;
 COLLISION=false;
 WIN=false;
 PLAYER_X_START=200;
 PLAYER_Y_START=383;
-/*
-const Enemy = function(y,speed) {
-    this.sprite = 'images/enemy-bug.png';
-    this.x=0;
-    this.y=y;
-    this.speed=speed;
 
-};
-*/
-
+// Enemies our player must avoid
 class Enemy {
     constructor({x,y,speed}){
         this.sprite = 'images/enemy-bug.png';
@@ -34,8 +26,8 @@ class Enemy {
     }
 
     isCollision(){
-        if (  (this.x > player.x-40 && this.x < player.x+40) &&
-              (this.y > player.y-30 && this.y < player.y+30) ) {
+        if (  (this.x > player.x-50 && this.x < player.x+50) &&
+              (this.y > player.y-40 && this.y < player.y+40) ) {
             return true;
         }
         else { 
@@ -46,47 +38,18 @@ class Enemy {
     render(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-
 }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-/*
-Enemy.prototype.update = function(dt) {
-    this.x+=dt*this.speed;
-    if (this.x>500){this.x=-100;}
-
-    if (  (this.x > player.x-50 && this.x < player.x+50) &&
-          (this.y > player.y-30 && this.y < player.y+30) ) {
-        COLLISION=true;
-        console.log(COLLISION);
-        setTimeout(()=>{
-            player.x=PLAYER_X_START;
-            player.y=PLAYER_Y_START;   
-            COLLISION=false;     
-        },500);
-
-    }
-};
-*/
 // Draw the enemy on the screen, required method for game
-/*
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-*/
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 //let Player={};
 //Player.__proto__=Enemy;
-/*
-const Player = function() {
-    this.sprite = 'images/char-boy.png';
-    this.x=PLAYER_X_START;
-    this.y=PLAYER_Y_START;
-};
-*/
+
 class Player {
     constructor(){
         this.sprite = 'images/char-boy.png';
@@ -141,49 +104,9 @@ class Player {
     }
 }
 
-/*
-Player.prototype.update = function(dt) {
-
-};
-
-Player.prototype.handleInput = function(dt) {
-    if (!COLLISION)
-        switch(dt){
-            case "up":this.y-=CELL_HEIGHT; 
-                if(this.y<0){
-                    WIN=true;alert("win");
-                    this.x=PLAYER_X_START;
-                    this.y=PLAYER_Y_START;
-                }
-                
-                break;
-            case "down":this.y+=CELL_HEIGHT;
-                if(this.y>400){
-                    this.y-=CELL_HEIGHT;
-                }    
-                break;
-            case "left":this.x-=CELL_WIDTH;
-                if(this.x<0){
-                    this.x+=CELL_WIDTH;
-                }    
-                break;
-            case "right":this.x+=CELL_WIDTH;
-                if(this.x>400){
-                    this.x-=CELL_WIDTH;
-                }  
-                break;
-            default:break;
-        }
-};
-*/
 // Draw the enemy on the screen, required method for game
-/*
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-*/
-// Now instantiate your objects.
 
+// Now instantiate your objects.
 
 class Game {
     constructor(){
@@ -205,7 +128,6 @@ class Game {
 
     }
 }
-
 
 const bug1=new Enemy({x:0,y:140,speed:100});
 const bug2=new Enemy({x:0,y:50,speed:50});
